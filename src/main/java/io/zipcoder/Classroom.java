3e86740663f69246e6159364cb32c297267e3235
Nyoxide <nyoxidetwitter@gmail.com>
2020-02-19 16:44:36 -0500

    made some stuff

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,87 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Classroom {
+
+    private Student[] students;
+    private TreeMap<Student, Double> treeHouse;
+
+    public Classroom(int maxNumberOfStudents) {
+        students = new Student[maxNumberOfStudents];
+        treeHouse = new TreeMap<>();
+    }
+
+    public Classroom(Student[] arr) {
+        treeHouse = new TreeMap<>();
+        students = new Student[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            students[i] = arr[i];
+        }
+    }
+
+    public Classroom() {
+        students = new Student[30];
+        treeHouse = new TreeMap<>();
+    }
+
+    public Student[] getStudents() {
+        return this.students;
+    }
+
+    public Double getAverageExamScore() {
+        double sum = 0.0f;
+        double noOf = 0.0f;
+        for (Student s : this.students) {
+            for (Double d : s.getTestScores()) {
+                sum += d;
+                noOf++;
+            }
+        }
+
+        return sum/noOf;
+    }
+
+    public void addStudent(Student student) {
+        this.students = new Student[this.students.length-1];
+        this.students[this.students.length - 1] = student;
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+        Student[] newArr = new Student[this.students.length - 1];
+        int newIndex = 0;
+        for (int i = 0; i < this.students.length; i++) {
+            if (!(this.students[i].getFirstName().equals(firstName) && this.students[i].getLastName().equals(lastName))) {
+                newArr[newIndex] = this.students[i];
+                newIndex++;
+            }
+        }
+    }
+
+    private void updateTreehouse() {
+        this.treeHouse.clear();
+        for (Student s : this.students) {
+            this.treeHouse.put(s, s.getAverageExamScore());
+        }
+    }
+
+    public Student[] getStudentsByScore() {
+        updateTreehouse();
+        Student[] toRet = new Student[this.students.length];
+        int index = 0;
+        for (Map.Entry<Student, Double> i : this.treeHouse.entrySet()) {
+            toRet[index] = i.getKey();
+        }
+        return toRet;
+    }
+
+    public Map<Student, Character> getLetterGrade() {
+        Map<Student, Character> toRet = new HashMap<>();
+
+        return toRet;
+    }
+
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,86 @@
 package io.zipcoder;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Student implements Comparable<Student> {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.examScores = new ArrayList<>();
+        this.examScores.addAll(Arrays.asList(testScores));
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getTestScores() {
+        return examScores;
+    }
+
+    public Integer getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public String getExamScores() {
+        String toRet = "";
+        int counter = 1;
+        for (Double d : this.examScores) {
+            toRet += "Exam " + counter + " -> " + d + "\n";
+        }
+        return toRet;
+    }
+
+    public void addExamScore(Double d) {
+        this.examScores.add(d);
+    }
+
+    public void setExamScore(int examNumber, Double newScore) {
+        if (examNumber < this.examScores.size()) {
+            this.examScores.set(examNumber, newScore);
+        }
+    }
+
+    public Double getAverageExamScore() {
+        Double sum = 0.0;
+        int noOf = this.examScores.size();
+        for (Double d : this.examScores) {
+            sum += d;
+        }
+        return sum/noOf;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName + "\n> Average Score: " + getAverageExamScore() + "\n> Exam Scores: " + getExamScores();
+    }
+
+
+    @Override
+    public int compareTo(Student o) {
+        if (((Student) o).getAverageExamScore() > this.getAverageExamScore()) {
+            return -1;
+        } else if (((Student) o).getAverageExamScore() < this.getAverageExamScore()) {
+            return 1;
+        } else {
+            return (this.getFirstName() + this.getLastName()).compareTo(o.getFirstName() + o.getLastName());
+        }
+    }
 }
